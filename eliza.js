14c7d939d3075c390242b840a99f11191e4ddcd5
Ajith Ranka <ajithranka@users.noreply.github.com>
2015-12-12 14:31:19 +0530

    Expand synonyms in decomp rules

--- eliza.js
+++ eliza.js
@@ -5,13 +5,38 @@ function Eliza(script) {
 
 Eliza.prototype.parseScript = function() {
   var keys = this.script.keys;
+  var synonyms = this.script.synonyms;
 
-  // Convert decomp rules to regular expressions
+  // Collapse synonymns to a regular expression
+  for (var syn in synonyms)
+    synonyms[syn] = "(" + syn + "|" + synonyms[syn].join("|") + ")";
+
   for (var i = 0; i < keys.length; i++) {
     var decomp = keys[i]["decomp"];
 
     for (var j = 0; j < decomp.length; j++) {
       var rule = decomp[j]["rule"];
+      
+      // Convert decomp rules to regular expressions
+      // (1) Expand synonyms
+      // (2) Expand asterisks
+      // (3) Expand whitespace
+
+      // Expand synonyms
+      var hasSynonym = /@(\S+)/;
+
+      var match = rule.match(hasSynonym);
+      var left = "", right = rule;
+      while (match) {
+        var leftEnd = match.index;
+        var rightStart = leftEnd + match[1].length + 1;
+
+        left += right.substring(0, leftEnd);
+        left += synonyms[match[1]];
+        right = right.substring(rightStart, right.length);
+        match = right.match(hasSynonym);
+      }
+      rule = left + right;
 
       // Expand asterisks
       var hasAsterisk = /(\S*)(\s*\*\s*)(\S*)/;
@@ -42,17 +67,21 @@ Eliza.prototype.parseScript = function() {
 
 // Code for testing
 var test_script = {
+  "synonyms": {
+    "bar": ["feel", "think", "believe", "wish"],
+    "baz": ["everybody", "nobody", "noone"]  
+  },
   "keys": [
     {
       "word": "no",
       "weight": 1,
       "decomp": [
         { "rule": "*" },
-        { "rule": "foo bar" }, 
-        { "rule": "foo * bar"}, 
-        { "rule": "* foo bar *" },
-        { "rule": "* foo * bar *" },
-        { "rule": "foo bar * baz boh" }
+        { "rule": "foo @bar" }, 
+        { "rule": "foo * @bar"}, 
+        { "rule": "* foo @bar *" },
+        { "rule": "* foo * @bar *" },
+        { "rule": "foo @bar * @baz boh" }
       ]
     }
   ]
